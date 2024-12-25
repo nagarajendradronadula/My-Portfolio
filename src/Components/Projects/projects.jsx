91f04d22dd18efc8bfe227d99c705ab4c020186b
NAGA RAJENDRA DRONADULA <127020330+nagarajendradronadula@users.noreply.github.com>
2024-12-25 17:51:07 +0530

    added portfolio as a project in project.jsx

--- src/Components/Projects/projects.jsx
+++ src/Components/Projects/projects.jsx
@@ -8,6 +8,8 @@ const Projects = () => {
         <div className="py-12 px-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             <ProjectCard title="WanderLust - Airbnb Clone" main="This is an Airbnb Clone called WanderLust and this has been built using Node.js, Express.js and MongoDB along with some libraries like passport and routers , etc., which has been deployed on Render." gitHubLink="https://github.com/nagarajendradronadula/MajorProject.git" liveLink="http://majorproject-bv6r.onrender.com/"/>
 
+            <ProjectCard title="DNR - Portfolio" main="This project is a personal portfolio website built with React, featuring various sections such as Home, About, Skills, Projects, and Footer." githubLink="https://github.com/nagarajendradronadula/My-Portfolio" liveLink="https://dnr-portfolio.vercel.app/" />
+
             <ProjectCard title="Weather App" main="This is a weather app built using HTML, CSS and JavaScript with an intuitive user interface where a user can search for any city and get the weather details." gitHubLink="https://github.com/nagarajendradronadula/MyGithub-Repo/tree/5cbadbb85c561866b45ee2eb3144018690a0101c/Projects/weather%20app" />
 
             <ProjectCard title="Chat With Gemini" main="This is a chat application built using Node.js, Express.js, MongoDB and Google Gemini API where it has an intuitive user interface where a user can ask for any query and get the response from Google's Gemini through the customised interface." gitHubLink="https://github.com/nagarajendradronadula/MyGithub-Repo/tree/5cbadbb85c561866b45ee2eb3144018690a0101c/Projects/chatWithAI" />
