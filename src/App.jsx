dadf3afa6dbd723e8e4ccc3afa5489e641b2b847
NAGA RAJENDRA DRONADULA <127020330+nagarajendradronadula@users.noreply.github.com>
2025-02-19 12:54:57 +0530

    done some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import Navbar from "./Components/Navbar/navbar.jsx";
 import Home from "./Components/Home/home.jsx";
-import About from "./Components/About/about.jsx";
-import Skills from "./Components/Skills/skills.jsx";
-import Projects from "./Components/Projects/projects.jsx";
-import Footer from "./Components/Footer/footer.jsx";
-import SimonGame from "./Components/SimonGame/simonGame.jsx";
-import Ads from "./Components/Ads/ads.jsx";
+import About from "./Components/About/about.jsx"
+import Skills from "./Components/Skills/skills.jsx"
+import Projects from "./Components/Projects/projects.jsx"
+import Footer from "./Components/Footer/footer.jsx"
+import SimonGame from "./Components/SimonGame/simonGame.jsx"
 
 function App() {
   
@@ -13,16 +12,13 @@ function App() {
     <div className="bg-[#171d32] h-auto w-full overflow-hidden">
       <Navbar />
       <Home />
-      <Ads />
       <About />
-      <Ads />
       <Skills />
-      <Ads />
       <SimonGame />
-      <Ads />
       <Projects />
-      <Ads />
       <Footer />
+
+      
     </div>
   )
 }
