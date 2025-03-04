2cca036a5bf2f835559c3be9b0324c058f5b136d
NAGA RAJENDRA DRONADULA <127020330+nagarajendradronadula@users.noreply.github.com>
2025-03-04 10:11:35 +0530

    Update home.jsx for resume download button
    new resume updated

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -1,6 +1,6 @@
 
 import profileImage from "../../assets/IMG_20220813_192157-modified-removebg-preview-modified.png";
-import Resume from "../../assets/DNR-Resume-4.pdf"
+import Resume from "../../assets/DNR-Resume-EnhancedwExp - Google Docs.pdf"
 
 const Home = () => {
   return (
@@ -12,7 +12,7 @@ const Home = () => {
         <p className="text-sm md:text-2xl tracking-tight mt-5">
           FULL STACK DEVELOPER | JAVA | PYTHON
         </p>
-        <a href={Resume} download="DNR-Resume-4.pdf">
+        <a href={Resume} download="DNR-Resume.pdf">
         <button className="mt-5 md:md-10 text-black py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697] border-solid bg-[#CAC9C7] p-3">            Here&apos;s My Resume
           </button>
         </a>
